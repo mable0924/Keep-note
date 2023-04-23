efb8c36d3093e401db200d8d032c493cbebf2492
mable0924 <pohinma@yahoo.com.hk>
2023-04-23 22:55:57 +0800

    Initial Commit

---
+++ Draggable/DraggableApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraggableApp.swift
+//  Draggable
+//
+//  Created by Po hin Ma on 23/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct DraggableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
